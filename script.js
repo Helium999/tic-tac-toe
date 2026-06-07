c883e33aa1a28e38ede23c34fb0fb19498140a4b
Helium <130857426+Helium999@users.noreply.github.com>
2026-06-06 22:31:29 -0700

    Create a Players object with a Player constructor and methods to create Player 1 and 2.

--- script.js
+++ script.js
@@ -21,3 +21,24 @@ const Gameboard = {
     }
 }
 
+const Players = {
+    Player: function(name, marker, score) {
+        this.name = name;
+        this.marker = marker;
+        this.score = score;
+    },
+
+    createPlayer1(name, marker, score) {
+        player1 = new Players.Player(name, marker, score);
+    },
+
+    createPlayer2(name, marker, score) {
+        if(name === player1.name) {
+            return "Choose a different name than Player1."
+        }
+        if(marker === player1.marker) {
+            return "Player 1 already has this marker."
+        }
+        player2 = new Players.Player(name, marker, score);
+    }
+}
